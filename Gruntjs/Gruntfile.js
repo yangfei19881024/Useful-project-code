7680f462dd89b0d5a74f7be053a70b42fcb0b92b
yangfei19881024@sina.com <yf19881024>
2015-01-21 10:33:58 +0800

    动态文件

--- Gruntjs/Gruntfile.js
+++ Gruntjs/Gruntfile.js
@@ -4,23 +4,32 @@ module.exports = function(grunt) {
         pkg: grunt.file.readJSON('package.json'),
 
         uglify: {
-
-          my_target: {
-            options:{
-                expand:true
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n' //添加banner
             },
-            files: {
-              'static/home/bulid/': ['static/home/src/*.js']
-            }
-          }
+            buildall: {
+                options: {
+                    mangle: true, //不设置混淆变量
+                    compress: {
+                        drop_console: true
+                    },
+                    report: "min" //输出压缩率，可选的值有 false(不输出信息)，gzip
+                },
+                files: [{
+                    expand:true,
+                    cwd:'static/',
+                    src: ['**/src/*.js'], //所有js文件
+                    dest: 'static/bulid/' //输出到此目录下
+                }]
+            },
 
-        }
+        },
 
     });
 
     grunt.loadNpmTasks('grunt-contrib-uglify');
 
-    grunt.registerTask('default', ['uglify:my_target']);
+    grunt.registerTask('default', ['uglify']);
     /*grunt.registerTask('unittest', ['connect', 'qunit']);
     grunt.registerTask('test', ['jshint', 'qunit']);
     grunt.registerTask('full', ['jshint', 'qunit', 'concat', 'uglify']);
---
+++ Gruntjs/static/bulid/home/src/home.js
@@ -0,0 +1,2 @@
+/*! grunt-js 2015-01-21 */
+!function(){function a(){for(var a=0;10>a;a++)document.getElementById("btn").onclick=function(){document.getElementById("div").innerHTML=a}(a)}a()}(home);
